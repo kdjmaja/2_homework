1f50991143a59475e52f6b7f46efaaae991e8faa
kdjmaja <maja.krmpotic-durdevic@fer.hr>
2016-10-29 23:20:03 +0200

    raupjc

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # 2_homework
 RAUPJC
+
+
+#Pitanje 1
+
+Izvođenje programa trajalo je 5.0013594 sekundi.
+
+#Pitanje 2
+
+Operacije A, B, C, D i E izvodile su se na jednoj dretvi.
+
+#Pitanje 3
+
+Izvođenje programa sada je trajalo 1.0868978 sekundi.
+
+#Pitanje 4
+
+Operacije A, B, C, D i E sada su se izvodile na 5 dretvi, svaka na svojoj.
+
+#Pitanje 5
+
+Paralelna petlja trajala je 1.9316 ms, a obična 0.0081 ms.
--- Solution_hw/Zad_6/Program.cs
+++ Solution_hw/Zad_6/Program.cs
@@ -1,15 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
-
+    
 namespace Zad_6
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            Parallel.For(0, 1000, (i) =>
+            {
+                var x = 2;
+                var y = 2;
+                var sum = x + y;
+            });
+            stopwatch.Stop();
+            Console.WriteLine(" Parallel calls finished {0} ms.",
+            stopwatch.Elapsed.TotalMilliseconds);
+            stopwatch.Restart();
+            for (int i = 0; i < 1000; i++)
+            {
+                int x = 2;
+                int y = 2;
+                int sum = x + y;
+            }
+            stopwatch.Stop();
+            Console.WriteLine(" Sync operation calls finished {0} ms.",
+            stopwatch.Elapsed.TotalMilliseconds);
+            Console.ReadLine();
+
+        }
+
+        public static void LongOperation(string taskName)
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine("{0} Finished . Executing Thread : {1}", taskName, Thread.CurrentThread.ManagedThreadId);
         }
     }
 }
